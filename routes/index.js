1a8c976907d7b4f9d70147e4166c1ce5d5fe2619
Jeff Eggink <jeffeggink@calvisitor-10-105-173-239.calvisitor.1918.berkeley.edu>
2018-08-11 13:15:43 -0700

    2legit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var tabledata = require('../data/tableData')
+var waitingdata = require('../data/tableData')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { tabledata:tabledata, waitingdata:waitingdata});
 });
+router.post('/api/adduser', function(req, res) {
+ var user = req.body
+ if (tabledata.length >= 5){
+  waitingdata.push (user)
+ }
+ else {
+  tabledata.push (user)
+ }
+ res.redirect('/');
+});
+
 
 module.exports = router;
